e345d4ab86bfa71c96cc7da0120e2b3b17d42cfd
Logof <Logof2008@mail.ru>
2026-05-15 15:06:45 +0300

    Create mod.rs

---
+++ src/ui/mod.rs
@@ -0,0 +1,7 @@
+pub mod map_editor;
+pub mod script_ide;
+pub mod configurator;
+
+pub use map_editor::render_map_editor;
+pub use script_ide::render_script_editor;
+pub use configurator::render_configurator;
